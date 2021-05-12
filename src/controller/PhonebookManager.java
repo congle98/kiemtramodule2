167c63785f63e9c368d28014908b8ab626728dae
congle98 <79907265+congle98@users.noreply.github.com>
2021-05-12 10:28:58 +0700

    up

---
+++ src/controller/PhonebookManager.java
@@ -0,0 +1,178 @@
+package controller;
+
+import model.Contact;
+import storage.TextFileFactory;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PhonebookManager {
+    private Scanner scanner = new Scanner(System.in);
+    private ArrayList<Contact> contacts = new ArrayList<Contact>();
+    private TextFileFactory textFileFactory = TextFileFactory.getINSTANCE();
+
+
+
+    public String createPhoneNumber(){
+        System.out.println("mời nhập số điện thoại");
+        String phoneNumber = scanner.nextLine();
+        return phoneNumber;
+    }
+    public String createGroup(){
+        System.out.println("mời nhập nhóm danh bạ ví dụ: gia đình, công việc");
+        String group = scanner.nextLine();
+        return group;
+    }
+    public String createFullName(){
+        System.out.println("mời nhập họ tên");
+        String fullName = scanner.nextLine();
+        return fullName;
+    }
+    public String createGender(){
+        System.out.println("mời nhập giới tính");
+        String gender = scanner.nextLine();
+        return gender;
+    }
+    public String createAddress(){
+        System.out.println("mời nhập địa chỉ");
+        String address = scanner.nextLine();
+        return address;
+    }
+    public String createDoB(){
+        System.out.println("mời nhập ngày sinh");
+        String dateOfBirth = scanner.nextLine();
+        return dateOfBirth;
+    }
+    public String createEmail(){
+        System.out.println("mời nhập Email");
+        String email = scanner.nextLine();
+        return email;
+    }
+    public void addContact(){
+        Contact contact = new Contact();
+        contact.setPhoneNumber(createPhoneNumber());
+        contact.setContactGroup(createGroup());
+        contact.setFullName(createFullName());
+        contact.setGender(createGender());
+        contact.setAddress(createAddress());
+        contact.setDateOfBirth(createDoB());
+        contact.setEmail(createEmail());
+        contacts.add(contact);
+    }
+//    public void showGroupContact(){
+//        for (Contact contact:contacts
+//             ) {
+//            System.out.println(contact.getContactGroup());
+//        }
+//    }
+    public void showContacts(){
+        for (Contact contact:contacts
+             ) {
+            System.out.println(contact);
+        }
+    }
+    public void showContactsMenu(){
+        Scanner scanner = new Scanner(System.in);
+        String choose;
+        do {
+            showContacts();
+            System.out.println("1.Thêm mới");
+            System.out.println("2.Thoát");
+            choose = scanner.nextLine();
+            switch (choose){
+                    case "1":
+                    addContact();
+                    break;
+                case "2":
+                    break;
+                default:
+                    System.err.println("sai tuỳ chọn");
+                    break;
+            }
+        }while (!choose.equals("2"));
+    }
+    public Contact getContactByPhoneNumber(){
+        System.out.println("mời nhập số điện thoại muốn sửa");
+        Boolean check = false;
+        Contact contact=null;
+        while (!check){
+            String numberPhone = scanner.nextLine();
+            for (Contact cont:contacts
+            ) {
+                if(cont.getPhoneNumber().equals(numberPhone)){
+                    contact = cont;
+                    check=true;
+                    break;
+                }
+            }
+            if(numberPhone.equals("")) break;
+            if(!check) System.out.println(" ko tìm thấy số điện thoại trên mời nhập lai");
+        }
+        return contact;
+    }
+    public void upadteContactMenu(){
+        Contact contact = getContactByPhoneNumber();
+        if(contact!=null){
+            Scanner scanner = new Scanner(System.in);
+            String choose;
+            System.out.println("Mời nhập thông tin muốn sửa");
+            do {
+                System.out.println("1.Nhóm danh bạ");
+                System.out.println("2.Họ và tên");
+                System.out.println("3.Giới tính");
+                System.out.println("4.Địa chỉ");
+                System.out.println("5.Ngày sinh");
+                System.out.println("6.Thoát");
+                choose = scanner.nextLine();
+                switch (choose){
+                    case "1":
+                        contact.setContactGroup(createGroup());
+                        break;
+                    case "2":
+                        contact.setFullName(createFullName());
+                        break;
+                    case "3":
+                        contact.setGender(createGender());
+                        break;
+                    case "4":
+                        contact.setAddress(createAddress());
+                        break;
+                    case "5":
+                        contact.setDateOfBirth(createDoB());
+                        break;
+                    case "6":
+                        break;
+                    default:
+                        System.err.println("sai tuỳ chọn");
+                        break;
+                }
+            }while (!choose.equals("6"));
+        }
+    }
+    public void deleteContact(){
+        Contact contact = getContactByPhoneNumber();
+        System.out.println("Nhấn Y để xoá hoặc phím bất kỳ để thoát");
+        String choose = scanner.nextLine();
+        if(choose.equalsIgnoreCase("y")){
+            contacts.remove(contact);
+        }
+    }
+    public void getContact(){
+        System.out.println("mời nhập số điện thoại cần tìm kiếm");
+        String phoneNumber = scanner.nextLine();
+        String []
+        for (int i = 0; i < phoneNumber.length(); i++) {
+        }
+    }
+    public void getDataInFile(){
+        contacts = textFileFactory.readerFile("data.txt");
+        if(contacts==null){
+            System.out.println("file chưa có dữ liệu!!");
+        }
+    }
+    public void saveDataOutFile(){
+        textFileFactory.saveFile(contacts,"data.txt");
+    }
+
+}
---
+++ src/views/Client.java
@@ -0,0 +1,62 @@
+package views;
+
+import controller.PhonebookManager;
+
+import java.util.Scanner;
+
+public class Client {
+    static PhonebookManager phonebookManager = new PhonebookManager();
+    public static void main(String[] args) {
+    menu();
+    }
+
+
+
+
+
+
+    static void menu(){
+        Scanner scanner = new Scanner(System.in);
+        String choose;
+        do {
+            System.out.println("1.Xem danh sách");
+            System.out.println("2.Thêm mới thuê bao");
+            System.out.println("3.Cập nhật");
+            System.out.println("4.Xoá liên hệ");
+            System.out.println("5.Tìm kiếm");
+            System.out.println("6.Đọc từ file");
+            System.out.println("7.Ghi từ file");
+            System.out.println("8.Thoát");
+            choose = scanner.nextLine();
+            switch (choose){
+                case "1":
+                    phonebookManager.showContactsMenu();
+                    break;
+                case "2":
+                    phonebookManager.addContact();
+                    break;
+                case "3":
+                    phonebookManager.upadteContactMenu();
+                    break;
+                case "4":
+                    phonebookManager.deleteContact();
+                    break;
+                case "5":
+
+                    break;
+                case "6":
+                    phonebookManager.getDataInFile();
+                    break;
+                case "7":
+                    phonebookManager.saveDataOutFile();
+                    break;
+                case "8":
+                    System.out.println("cảm ơn đã dùng phần mềm");
+                    break;
+                default:
+                    System.err.println("sai tuỳ chọn");
+                    break;
+            }
+        }while (!choose.equals("8"));
+    }
+}
